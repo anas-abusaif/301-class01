5171be0a7cf502eee5ab51541240f60b47e72a51
anas-abusaif <anasabusief@gmail.com>
2021-08-02 18:39:59 +0300

    solved lab 2

--- my-app/src/components/HornedBeast.js
+++ my-app/src/components/HornedBeast.js
@@ -1,13 +1,26 @@
 import react from 'react';
 class HornedBeast extends react.Component {
 
+  constructor(){
+    super();
+    this.state={
+      favorites:0
+    }
+  }
+pickFavorite=()=>{
+  let likes=this.state.favorites;
+  this.setState({
+    favorites:likes+1
+  })
+};
   render() {
     return (
-      <>
+      <div className='animal'>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imgUrl} alt={this.props.title}/>
+        <img src={this.props.imgUrl} alt={this.props.title} onClick={this.pickFavorite}/>
         <p>{this.props.description}</p>
-      </>
+       <p>favorites {this.state.favorites}</p>
+      </div>
     )
   }
 }
