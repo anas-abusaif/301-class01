1af62f5a94943c68eb70248f9336f4db8f253f56
anas-abusaif <anasabusief@gmail.com>
2021-08-03 15:33:36 +0300

    finished lab 2 and bits of lab 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,16 @@ import react from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends react.Component{
   render(){
     return(
       <>
         <Header/>
-        <div className='main'>
+        
         <Main/>
-        </div>
+        
         <Footer/>
       </>
     )
--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,11 +1,13 @@
 import react from 'react';
 import HornedBeast from './HornedBeast'
 import data from './data.json'
-
+import { Container, Row } from 'react-bootstrap';
 class Main extends react.Component {
   render() {
     return (
       <>
+      <Container>
+        <Row>
         <HornedBeast title='short horned buffalo' imgUrl='https://www.africa-wildlife-detective.com/images/buffalo-800-2.jpg' description='The Short-horned Water Buffalo (Bubalus mephistopheles) is an extinct species of Water Buffalo.' />
 
         <HornedBeast title='Horned desert viper' imgUrl='https://i.pinimg.com/originals/bf/8d/37/bf8d3726824b82c4d9e4a949e7eca29a.jpg' description='The horned viper is a poisonous snake that is active at dusk and during the night.' />
@@ -14,6 +16,8 @@ class Main extends react.Component {
             return <HornedBeast title={element.title} imgUrl={element.image_url} description={element.description} />
           })
         }
+        </Row>
+        </Container>
       </>
     )
   }
